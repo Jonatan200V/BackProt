9fc11603ab1125c117bddd2c11c244b756c596e9
Jonatan <jonatanangel88@gmail.com>
2023-01-21 03:49:57 -0300

    email-controller

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/mail.routes');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
